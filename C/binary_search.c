246874bf97923fd324cf02888bd81ab730744618
Sachin Kumar <koolsam53@gmail.com>
2022-10-16 15:24:45 +0530

    Now User can take inputs in code

--- C/binary_search.c
+++ C/binary_search.c
@@ -31,13 +31,20 @@ int binarySearch(int arr[], int l, int r, int x)
 
 int main(void)
 {
-	int arr[] = { 2, 3, 4, 10, 40 };
-	int n = sizeof(arr) / sizeof(arr[0]);
-	int x = 10;
+	int n,i,x,arr[50];
+	printf("Enter the size of the list: ");
+	scanf("%d",&n);
+	printf("Enter the list of Elements:");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	printf("Enter the element to Search:");
+	scanf("%d",&x);
 	int result = binarySearch(arr, 0, n - 1, x);
 	(result == -1)
 		? printf("Element is not present in array")
-		: printf("Element is present at index %d", result);
+		: printf("Element is present at index---> %d", result);
 	return 0;
 }
 
